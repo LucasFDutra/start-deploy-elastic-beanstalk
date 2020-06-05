5d5c0927edae3fb27dd79ac9219f8c21ec8cf637
LucasFDutra <lucasfelipedutra@gamil.com>
2020-06-05 14:37:33 -0300

    test change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const routes = express.Router();
 routes.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('hello my world');
 });
 
 app.use(routes);
