227c7ef4315bdd19d46fe4256bf15dea9c46f551
LucasFDutra <lucasfelipedutra@gamil.com>
2020-06-05 14:28:42 -0300

    recomecei com uma aplicação simples

--- dist/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const routes = express_1.Router();
-routes.get('/', (req, res) => {
-    res.send('Hello my world');
-});
-exports.default = routes;
